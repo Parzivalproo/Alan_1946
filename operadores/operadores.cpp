251dbbca06b90d0bfc52c9de01d2e8332be5817a
alan sanchez <aljisalo06@gmail.com>
2024-11-25 10:44:04 -0500

    operadores v1

--- operadores/operadores.cpp
+++ operadores/operadores.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
+
 main () {
+    //operadores relacionales
     int a = 5, b = 10;
-    cout << "a > b " << (a>b) << endl;
+    bool c= true, d = false;
+    /*cout << "a > b " << (a>b) << endl;
     cout << "a < b " << (a<b) << endl;
     cout << "a >= b " << (a>=b) << endl;
     cout << "a <= b " << (a<=b) << endl;
     cout << "a == b " << (a==b) << endl;
-    cout << "a != b " << (a!=b) << endl;
+    cout << "a != b " << (a!=b) << endl;*/
+
+    //operadores logicos
+    cout << boolalpha;
+    cout << "c && d: " << (c && d) << endl;
+    cout << "c || d: " << (c || d) << endl;
+    cout << "!c: " << !c << endl;
+    cout << "!d: " << !d << endl;
+    cout << "c && !d: " << (c && !d) << endl;
+    cout << "c || !d: " << (c || !d) << endl;
+    cout << "!c && d: " << (!c && d) << endl;
+    cout << "!c || d: " << (!c || d) << endl;
+
 
 }
