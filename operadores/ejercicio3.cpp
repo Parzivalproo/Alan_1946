d4c47bd55bf113d2edc87f5ed1ab33ecf7fee7f7
alan sanchez <aljisalo06@gmail.com>
2024-11-27 10:46:48 -0500

    uso del operador de incremento y decremento fin

--- operadores/ejercicio3.cpp
+++ operadores/ejercicio3.cpp
@@ -3,9 +3,18 @@
 using namespace std;
 main ()
 {
-    int n1=10, n2=20;
+    int n1=10, n2=20, resultado;
     n1++;//n1= n1 + 1
     ++n2;//n2= n2 + 1
     cout<<"n1= "<< n1<<endl;
     cout<<"n2= "<< n2<<endl;
+    //operaciones de preincremento
+    resultado = ++n1;
+    cout << "El resultado= "<< resultado<<endl;
+    resultado = n2++;
+    cout << "n2 >= " << n2 <<endl;
+    cout << "El resultado= "<< resultado<<endl;
+    
+
+
 }
