a6e5ad8495c94a10e79c87daa3cc9298e431bc04
alan sanchez <aljisalo06@gmail.com>
2024-11-27 09:40:31 -0500

    uso del operador ternario fin

--- operadores/ejercicio2.cpp
+++ operadores/ejercicio2.cpp
@@ -1,11 +1,23 @@
+//uso del operador ternario
 #include <iostream>
 using namespace std;
 main () {
-    int n1, n2;
+    int n1, n2, resultado;
+    bool operacion;
     cout << "Ingresa un numero ";
     cin >> n1;
     cout << "Ingresa otro numero ";
     cin >> n2;
     cout << "Primera forma: "<< endl;
     (n1>n2)?cout<< n1 <<" es mayor que  "<< n2 : cout << n2 << " es mayor que "<<n1;
+    cout << endl;
+    cout << "Segunda forma: "<< endl;
+    resultado = (n1>n2)?n1/n2:n1%n2;
+    cout << "el resultado es: "<< resultado<< endl;
+    operacion = (n1>n2)? true: false;
+    if (operacion)
+    cout << "el resultado guarda la de division de " << n1 << " entre " << n2 << endl;
+    else
+    cout << "El resultado guarda el modulo de " << n1 << " entre " << n2 << endl;
+
 }
