c6469280f002e4e726d3c53604c774d330f9b430
alan sanchez <aljisalo06@gmail.com>
2024-11-29 10:31:02 -0500

    uso del swtich y break con char

---
+++ Lenguaje_C/Selectiva/caracter.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main ()
+{
+    char dia;
+    cout << "Ingresa un caracter";
+    cin >> dia;
+    switch (dia)
+{
+    case 'L':
+    case 'l"':
+    cout << "Es lunes"<< endl;
+    break;
+    case 'M':
+    case 'm':
+    cout << "Es martes"<< endl;
+    break;
+}
+
+
+}
