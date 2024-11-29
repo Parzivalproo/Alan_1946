b782edafa48b1a69fd6048b403aee511c4f42d03
alan sanchez <aljisalo06@gmail.com>
2024-11-29 10:24:01 -0500

    uso del swtich y break

---
+++ Lenguaje_C/Selectiva/multiples.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+main ()
+{
+    int dia;
+    cout << "Ingrese el dia: ";
+    cin >> dia;
+    switch (dia)
+    {
+        case 1:
+                cout << "Es lunes"<< endl;
+                break;
+        case 2:
+                cout << "Es martes"<< endl;
+                break;
+        case 3:
+                cout << "Es miercoles"<< endl;
+                break;
+        case 4:
+                cout <<  "Es jueves"<< endl;
+                break;
+        case 5:
+                cout << "Es viernes"<< endl;
+                break;
+        case 6:
+        case 7:
+                cout << "Es fin de semana"<< endl;
+                break;
+        default:
+                cout << "Dia invalido"<< endl;
+}
+}
