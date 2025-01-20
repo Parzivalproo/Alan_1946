928edf77810d5b4b8c45053302058d53a718df69
alan sanchez <aljisalo06@gmail.com>
2025-01-19 23:24:28 -0500

    edicion de tipo float de entre 55 a 99

--- arrays/unidimensionales/ejercicio2.cpp
+++ arrays/unidimensionales/ejercicio2.cpp
@@ -2,28 +2,31 @@
 #include <stdlib.h>//libreria para el uso de rand
 #include <time.h>//libreria para el uso de time
 using namespace std;
-void llenaDatos (int v[], int ne){
+void llenaDatos (float v[], int ne){
     srand (time(NULL));//funcion semilla para generar numeros
     for (int i=0; i<ne; i++){
-        v[i]=rand()%20+1;
+       v[i] = (float)(rand() % 45 + 55) + (float)(rand() % 100) / 100; 
+       if (v[i]>99){
+           v[i]=99;
+           }
     }
     }
-void verDatos (int v[], int ne){
+void verDatos (float v[], int ne){
     cout <<"los elementos del vector son: "<<endl;
     for (int i=0; i<ne; i++){
         cout << v[i] <<" ";
     }
 }
-float sumaPromedioCifras (int v[], int ne){
-    int suma=0;
+float sumaPromedioCifras (float v[], int ne){
+    float suma=0;
     for (int i=0; i<ne; i++){
         suma+=v[i];
 }
 return (float)suma/ne;
 
 }
-int contarDebajoPromedio (int v[], int ne, float promedio){
-int contar =0;
+int contarDebajoPromedio (float v[], int ne, float promedio){
+float contar =0;
 for (int i=0; i<ne; i++){
     if (v[i]<promedio){
         contar++;
@@ -31,7 +34,7 @@ for (int i=0; i<ne; i++){
 }
 return contar;
 }
-void guardaElementosmenores (int v[], int ne, float promedio, int lista []){
+void guardaElementosmenores (float v[], int ne, float promedio, float lista []){
     int j=0;
     for (int i=0; i<ne; i++){
         if (v[i]<promedio){
@@ -45,14 +48,14 @@ int main () {
     float promedioNumeros;
     cout<< "Cuantos elemetos tiene el vector?: ";
     cin>>ne;
-    int vector [ne];
+    float vector [ne];
     llenaDatos (vector, ne);
     verDatos (vector, ne);
     promedioNumeros = sumaPromedioCifras (vector, ne);
     cout << "\nEl promedio de los datos generados es= "<< promedioNumeros <<endl;
     menores = contarDebajoPromedio (vector, ne, promedioNumeros);
     cout << "La cantidad de elementos debajo del promedio son: " << menores <<endl;
-    int listaMenores [menores];
+    float listaMenores [menores];
     guardaElementosmenores (vector, ne, promedioNumeros, listaMenores);
     verDatos (listaMenores, menores);
 }
