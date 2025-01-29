8562e4313ba19094a134bc3e9949d77879ca64c9
alan sanchez <aljisalo06@gmail.com>
2025-01-29 09:27:03 -0500

    correcion del array

--- arrays/cadenqa.cpp
+++ arrays/cadenqa.cpp
@@ -1,23 +1,20 @@
-#include <cstring> 
+#include <iostream>
+#include <cstring>
+using namespace std;
+
 string identificarTipo(const char array[]) {
     bool tieneDigitos = false;
     bool tieneLetras = false;
     bool tieneOtros = false;
 
-    
-    for (size_t i = 0; i < strlen(array); ++i) {
+    for (size_t i = 0; array[i] != '\0'; ++i) {
         char c = array[i];
 
-        
         if (c >= '0' && c <= '9') {
             tieneDigitos = true;
-        }
-        
-        else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+        } else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
             tieneLetras = true;
-        }
-    
-        else {
+        } else {
             tieneOtros = true;
         }
     }
@@ -26,9 +23,17 @@ string identificarTipo(const char array[]) {
         return "Numerica";
     } else if (tieneLetras && !tieneDigitos && !tieneOtros) {
         return "Alfabetica";
-    } else if ((tieneLetras || tieneDigitos) && !tieneOtros) {
+    } else if (tieneLetras && tieneDigitos && !tieneOtros) {
         return "Alfanumerica";
     } else {
         return "Contiene caracteres no alfanumericos";
     }
+}
+int main() {
+    char array[100];
+    cout << "Ingrese una cadena de caracteres: ";
+    cin.getline(array, 100);
+    string tipo = identificarTipo(array);
+    cout << "El tipo de cadena es: " << tipo << endl;
+    return 0;
 }
