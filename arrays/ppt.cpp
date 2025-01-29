fc26acd9e7e7287841f5fc5b96eb50dc51688a7f
alan sanchez <aljisalo06@gmail.com>
2025-01-29 09:18:47 -0500

    ganar ppt al llegar a 3 puntos

--- arrays/ppt.cpp
+++ arrays/ppt.cpp
@@ -4,9 +4,7 @@
 using namespace std;
 
 int maquina() {
-    srand(time(NULL));
-    int ne = (rand() % 3 + 1);
-    return ne;
+    return rand() % 3 + 1;
 }
 
 int usuario() {
@@ -14,40 +12,76 @@ int usuario() {
     cout << "1. Piedra" << endl;
     cout << "2. Papel" << endl;
     cout << "3. Tijera" << endl;
-    cout << "Ingrese su eleccion" << endl;
+    cout << "Ingrese su eleccion: ";
     cin >> ne;
     while (ne < 1 || ne > 3) {
-        cout << "Elección invalida. Por favor, ingrese un numero entre 1 y 3." << endl;
+        cout << "Eleccion invalida. Por favor, ingrese un numero entre 1 y 3: ";
         cin >> ne;
     }
     return ne;
 }
 
 void imprimirEleccion(int ne, string nombre) {
-    if (ne == 1) {
-        cout << nombre << "= Piedra" << endl;
-    } else if (ne == 2) {
-        cout << nombre << "= Papel" << endl;
-    } else if (ne == 3) {
-        cout << nombre << "= Tijera" << endl;
-    }
+    string opciones[] = {"Piedra", "Papel", "Tijera"};
+    cout << nombre << " = " << opciones[ne - 1] << endl;
 }
 
-void determinarGanadorRonda(int usuario, int maquina) {
+bool determinarGanador(int usuario, int maquina) {
     if (usuario == maquina) {
         cout << "Empate" << endl;
+        return false;
     } else if ((usuario == 1 && maquina == 3) || (usuario == 2 && maquina == 1) || (usuario == 3 && maquina == 2)) {
-        cout << "Usuario gana la ronda" << endl;
+        cout << "Usuario gana la ronda." << endl;
+        return true;
     } else {
-        cout << "Maquina gana la ronda" << endl;
+        cout << "Maquina gana la ronda." << endl;
+        return false;
     }
 }
 
 int main() {
-    int usuarioEleccion = usuario();
-    int maquinaEleccion = maquina();
-    imprimirEleccion(usuarioEleccion, "Usuario");
-    imprimirEleccion(maquinaEleccion, "Máquina");
-    determinarGanadorRonda(usuarioEleccion, maquinaEleccion);
+    srand(time(NULL));
+    int puntosUsuario = 0, puntosMaquina = 0;
+    int dificultad;
+    
+    cout << "Seleccione la dificultad: \n1. Facil \n2. Medio \n3. Dificil" << endl;
+    cin >> dificultad;
+    while (dificultad < 1 || dificultad > 3) {
+        cout << "Dificultad invalida. Por favor, ingrese un numero entre 1 y 3: ";
+        cin >> dificultad;
+    }
+    
+    if (dificultad == 2) {
+        puntosMaquina = 1;
+    } else if (dificultad == 3) {
+        puntosMaquina = 2;
+    }
+
+    while (puntosUsuario < 3 && puntosMaquina < 3) {
+        int usuarioEleccion = usuario();
+        int maquinaEleccion = maquina();
+
+        imprimirEleccion(usuarioEleccion, "Usuario");
+        imprimirEleccion(maquinaEleccion, "Maquina");
+        cout << "**********************************" << endl;
+
+        if (determinarGanador(usuarioEleccion, maquinaEleccion)) {
+            puntosUsuario++;
+        } else if (usuarioEleccion != maquinaEleccion) {
+            puntosMaquina++;
+        }
+
+        cout << "Puntos: Usuario " << puntosUsuario << " - Maquina " << puntosMaquina << endl;
+        cout << "**********************************" << endl;
+        
+        if (puntosUsuario == 3) {
+            cout << "\nUsuario gana el juego!" << endl;
+            break;
+        } else if (puntosMaquina == 3) {
+            cout << "\nMaquina gana el juego!" << endl;
+            break;
+        }
+    }
+
     return 0;
 }
