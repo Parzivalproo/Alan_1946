1b5571715bf7cba130fc7a6261e76ae9b4e134f8
alan sanchez <aljisalo06@gmail.com>
2025-02-05 10:12:52 -0500

    integracion de otros arrays de archivos cpp

--- Librerias/arrays.h
+++ Librerias/arrays.h
@@ -35,3 +35,71 @@ int ordenarSeleccion(int v[], int n) {
     }
     return contador;
 }
+int ordenarBurbujav1(int v[], int n) {
+    int aux, contador = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            contador++;
+            if (v[j] > v[j + 1]) {
+                aux = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = aux;
+            }
+        }
+    }
+    return contador;
+}
+
+int ordenarBurbujav2(int v[], int n) {
+    int mejora = 1;
+    int aux, contador = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - mejora; j++) {
+            contador++;
+            if (v[j] > v[j + 1]) {
+                aux = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = aux;
+            }
+        }
+        mejora++;
+    }
+    return contador;
+}
+
+int ordenarBurbujav3(int v[], int n) {
+    int mejora = 1;
+    bool cambio = true;
+    int aux, contador = 0;
+    for (int i = 0; i < n && cambio; i++) {
+        cambio = false;
+        for (int j = 0; j < n - mejora; j++) {
+            contador++;
+            if (v[j] > v[j + 1]) {
+                aux = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = aux;
+                cambio = true;
+            }
+        }
+        mejora++;
+    }
+    return contador;
+}
+int ordenarInsersion(int v[], int n) {
+    int contador = 0;
+    for (int i = 1; i < n; i++) {
+        int aux = v[i];
+        int j = i - 1;
+        while (j >= 0) {
+            contador++;
+            if (aux<v[j]) {
+                v[j + 1] = v[j];
+            v[j] = aux;
+        
+        }
+        j--;
+    }
+}
+return contador;
+}
